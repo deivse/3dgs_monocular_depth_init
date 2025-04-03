fcec1d975905bb3a49296dffef8c2ba521163451
Ivan Desiatov <desiaiva@cvut.cz>
2025-04-03 13:56:07 +0200

    run_viewer.sh improvements

--- run_viewer.sh
+++ run_viewer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <scene> <model>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <scene> <model> [<port=0>]"
     exit 1
 fi
 
 scene=$1
 model=$2
+port=${3:-0}
 
-nerfbaselines viewer --checkpoint nerfbaselines_results/$scene/$model/checkpoint-30000/ --data external://$scene --backend python --port 0
+RESULTS_DIR="${RESULTS_DIR:-nerfbaselines_results}"
+
+nerfbaselines viewer --checkpoint $RESULTS_DIR/$scene/$model/checkpoint-30000/ --data external://$scene --backend python --port $port
