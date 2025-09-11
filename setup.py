ee389c1290ae50d109cc74aeda8c2979aed693cb
Ivan Desiatov <desiaiva@fel.cvut.cz>
2025-09-11 16:25:25 +0000

    Bump gsplat to 1.5.2

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ NATIVE_MODULES_PATH = Path(__file__).parent / "native_modules"
 
 setup(
     install_requires=[
-
         f"pointcloud_subsampling @ {(NATIVE_MODULES_PATH / 'subsampling').as_uri()}",
         f"depth_pro @ {(THIRD_PARTY_PATH / 'apple_depth_pro').as_uri()}",
         "unidepth @git+https://github.com/lpiccinelli-eth/UniDepth.git@bebc4b2fdda5c223693c390739db76897aee42e5",
         "mmcv@https://download.openmmlab.com/mmcv/dist/cu121/torch2.4.0/mmcv-2.2.0-cp310-cp310-manylinux1_x86_64.whl",
         "fused_ssim@git+https://github.com/rahul-goel/fused-ssim@30fb258c8a38fe61e640c382f891f14b2e8b0b5a",
-        "gsplat @ https://github.com/nerfstudio-project/gsplat/releases/download/v1.4.0/gsplat-1.4.0%2Bpt22cu121-cp310-cp310-linux_x86_64.whl",
+        "gsplat @ https://github.com/nerfstudio-project/gsplat/releases/download/v1.5.2/gsplat-1.5.2%2Bpt22cu121-cp310-cp310-linux_x86_64.whl",
         "xformers~=0.0.24",
         "transformers~=4.49.0",
         "datetime~=5.5.0",
